9237632a26fb589a7af7b885013e2c64e153ad58
InvaderZ3ro <kevinleewillis@gmail.com>
2026-02-13 13:48:05 -0500

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,2 +1,7 @@
 (Get-Date).Date
 return "This is a test"
+
+#to invoke the script
+#  $scriptContent = Invoke-WebRequest -Uri "https://raw.githubusercontent.com/InvaderZ3ro/PublicRepo/refs/heads/main/test.ps1" -UseBasicParsing
+#  $scriptBlock = [Scriptblock]::Create($scriptContent.Content)
+#  Invoke-Command -ScriptBlock $scriptBlock
