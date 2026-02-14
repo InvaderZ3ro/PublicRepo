965fe2e57d18c3cf660045d369590a3c7fe8c83b
InvaderZ3ro <kevinleewillis@gmail.com>
2026-02-14 02:13:06 -0500

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,6 +1,7 @@
 (Get-Date).Date
 return "This is a test"
-
+$ip = ipconfig /all
+$ip | out-file -filepath "Y:\$env:computername.txt
 #to invoke the script
 #  $scriptContent = Invoke-WebRequest -Uri "https://raw.githubusercontent.com/InvaderZ3ro/PublicRepo/refs/heads/main/test.ps1" -UseBasicParsing
 #  $scriptBlock = [Scriptblock]::Create($scriptContent.Content)
