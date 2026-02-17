93d589508bbf059d59bed03d02ccbc02629a9a90
InvaderZ3ro <kevinleewillis@gmail.com>
2026-02-17 13:58:53 -0500

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -23,23 +23,13 @@ $banner = @"
                                                                                                                                                                 
 "@
 Write-Host $banner -ForegroundColor Cyan
-
 #Apply FFU
-Write-SectionHeader -Title 'Applying FFU'
-WriteLog "Applying FFU to $PhysicalDeviceID"
 $FFUFileToInstall = "Z:\Win11_current.ffu"
 $PhysicalDeviceID = "\\.\PhysicalDisk0"
-WriteLog "Running command dism /apply-ffu /ImageFile:$FFUFileToInstall /ApplyDrive:$PhysicalDeviceID"
 #In order for Applying Image progress bar to show up, need to call dism directly. Might be a better way to handle, but must have progress bar show up on screen.
 dism /apply-ffu /ImageFile:$FFUFileToInstall /ApplyDrive:$PhysicalDeviceID
 
 Write-Host "Setting Windows Boot Manager to be first in the firmware display order."
 Invoke-Process bcdedit.exe "/set {fwbootmgr} displayorder {bootmgr} /addfirst"
-WriteLog "Setting Windows Boot Manager to be first in the default display order."
 Write-Host "Setting Windows Boot Manager to be first in the default display order."
 Invoke-Process bcdedit.exe "/set {bootmgr} displayorder {default} /addfirst"
-
-
-
-
-
