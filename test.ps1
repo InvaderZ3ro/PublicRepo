c648cbe0951f43855fc23506b11fe43029d74e0f
InvaderZ3ro <kevinleewillis@gmail.com>
2026-02-17 14:00:25 -0500

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -24,8 +24,8 @@ $banner = @"
 "@
 Write-Host $banner -ForegroundColor Cyan
 #Apply FFU
-$FFUFileToInstall = "Z:\Win11_current.ffu"
-$PhysicalDeviceID = "\\.\PhysicalDisk0"
+$FFUFileToInstall = '$USBDrive\Win11_current.ffu'
+$PhysicalDeviceID = '\\.\PhysicalDisk0'
 #In order for Applying Image progress bar to show up, need to call dism directly. Might be a better way to handle, but must have progress bar show up on screen.
 dism /apply-ffu /ImageFile:$FFUFileToInstall /ApplyDrive:$PhysicalDeviceID
 
