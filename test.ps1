b53115225743a0895e2757d7904a2500bc25b962
InvaderZ3ro <kevinleewillis@gmail.com>
2026-02-17 14:03:25 -0500

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -2,26 +2,10 @@ $ip = ipconfig /all
 $ip | out-file -filepath "Z:\$env:computername.txt
 
 $USBDrive = "Z\:"
-
-# Display banner and version
-$banner = @"
-
-███████╗███████╗██╗   ██╗    ██████╗ ██╗   ██╗██╗██╗     ██████╗ ███████╗██████╗ 
-██╔════╝██╔════╝██║   ██║    ██╔══██╗██║   ██║██║██║     ██╔══██╗██╔════╝██╔══██╗
-█████╗  █████╗  ██║   ██║    ██████╔╝██║   ██║██║██║     ██║  ██║█████╗  ██████╔╝
-██╔══╝  ██╔══╝  ██║   ██║    ██╔══██╗██║   ██║██║██║     ██║  ██║██╔══╝  ██╔══██╗
-██║     ██║     ╚██████╔╝    ██████╔╝╚██████╔╝██║███████╗██████╔╝███████╗██║  ██║
-╚═╝     ╚═╝      ╚═════╝     ╚═════╝  ╚═════╝ ╚═╝╚══════╝╚═════╝ ╚══════╝╚═╝  ╚═╝
-                                                                                                                                                                
-"@
-Write-Host $banner -ForegroundColor Cyan
 #Apply FFU
 $FFUFileToInstall = '$USBDrive\Win11_current.ffu'
 $PhysicalDeviceID = '\\.\PhysicalDisk0'
 #In order for Applying Image progress bar to show up, need to call dism directly. Might be a better way to handle, but must have progress bar show up on screen.
 dism /apply-ffu /ImageFile:$FFUFileToInstall /ApplyDrive:$PhysicalDeviceID
-
-Write-Host "Setting Windows Boot Manager to be first in the firmware display order."
 Invoke-Process bcdedit.exe "/set {fwbootmgr} displayorder {bootmgr} /addfirst"
-Write-Host "Setting Windows Boot Manager to be first in the default display order."
 Invoke-Process bcdedit.exe "/set {bootmgr} displayorder {default} /addfirst"
